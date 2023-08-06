cba9d4b279d3d4a17ed7f3665402009d45700571
Fathi <fathi.mussa@hollaus.at>
2023-08-06 16:28:43 +0200

    Legion Tracker added with Notification

--- D4_Tools/FMain.Designer.vb
+++ D4_Tools/FMain.Designer.vb
@@ -24,15 +24,16 @@ Partial Class FMain
 	Private Sub InitializeComponent()
 		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FMain))
 		Me.pMenu = New System.Windows.Forms.Panel()
-		Me.btnSkill = New System.Windows.Forms.Button()
 		Me.btnMap = New System.Windows.Forms.Button()
-		Me.btnDungeon = New System.Windows.Forms.Button()
+		Me.btnSkill = New System.Windows.Forms.Button()
+		Me.pbLogo = New System.Windows.Forms.PictureBox()
 		Me.btnTracker = New System.Windows.Forms.Button()
+		Me.btnDungeon = New System.Windows.Forms.Button()
 		Me.pHomeContent = New System.Windows.Forms.Panel()
 		Me.Panel5 = New System.Windows.Forms.Panel()
-		Me.lblCountdownBossFive = New System.Windows.Forms.Label()
+		Me.lblCountdownHelltideOne = New System.Windows.Forms.Label()
 		Me.Panel4 = New System.Windows.Forms.Panel()
-		Me.lblCountdownBossFour = New System.Windows.Forms.Label()
+		Me.lblCountdownLegionOne = New System.Windows.Forms.Label()
 		Me.Panel3 = New System.Windows.Forms.Panel()
 		Me.lblCountdownBossThree = New System.Windows.Forms.Label()
 		Me.Panel2 = New System.Windows.Forms.Panel()
@@ -49,8 +50,12 @@ Partial Class FMain
 		Me.PictureBox4 = New System.Windows.Forms.PictureBox()
 		Me.PictureBox3 = New System.Windows.Forms.PictureBox()
 		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-		Me.pbLogo = New System.Windows.Forms.PictureBox()
+		Me.Panel6 = New System.Windows.Forms.Panel()
+		Me.lblCountdownLegionTwo = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
 		Me.pMenu.SuspendLayout()
+		CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.pHomeContent.SuspendLayout()
 		Me.Panel5.SuspendLayout()
 		Me.Panel4.SuspendLayout()
@@ -62,7 +67,8 @@ Partial Class FMain
 		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
-		CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.Panel6.SuspendLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
 		'
 		'pMenu
@@ -80,21 +86,6 @@ Partial Class FMain
 		Me.pMenu.Size = New System.Drawing.Size(782, 43)
 		Me.pMenu.TabIndex = 0
 		'
-		'btnSkill
-		'
-		Me.btnSkill.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-		Me.btnSkill.FlatAppearance.BorderSize = 0
-		Me.btnSkill.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.btnSkill.Font = New System.Drawing.Font("Arial", 9.75!)
-		Me.btnSkill.ForeColor = System.Drawing.Color.White
-		Me.btnSkill.Location = New System.Drawing.Point(105, 0)
-		Me.btnSkill.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
-		Me.btnSkill.Name = "btnSkill"
-		Me.btnSkill.Size = New System.Drawing.Size(115, 42)
-		Me.btnSkill.TabIndex = 4
-		Me.btnSkill.Text = "Skill"
-		Me.btnSkill.UseVisualStyleBackColor = False
-		'
 		'btnMap
 		'
 		Me.btnMap.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
@@ -110,20 +101,32 @@ Partial Class FMain
 		Me.btnMap.Text = "Map"
 		Me.btnMap.UseVisualStyleBackColor = False
 		'
-		'btnDungeon
+		'btnSkill
 		'
-		Me.btnDungeon.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-		Me.btnDungeon.FlatAppearance.BorderSize = 0
-		Me.btnDungeon.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.btnDungeon.Font = New System.Drawing.Font("Arial", 9.75!)
-		Me.btnDungeon.ForeColor = System.Drawing.Color.White
-		Me.btnDungeon.Location = New System.Drawing.Point(555, 0)
-		Me.btnDungeon.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
-		Me.btnDungeon.Name = "btnDungeon"
-		Me.btnDungeon.Size = New System.Drawing.Size(115, 42)
-		Me.btnDungeon.TabIndex = 2
-		Me.btnDungeon.Text = "Dungeon"
-		Me.btnDungeon.UseVisualStyleBackColor = False
+		Me.btnSkill.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+		Me.btnSkill.FlatAppearance.BorderSize = 0
+		Me.btnSkill.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.btnSkill.Font = New System.Drawing.Font("Arial", 9.75!)
+		Me.btnSkill.ForeColor = System.Drawing.Color.White
+		Me.btnSkill.Location = New System.Drawing.Point(105, 0)
+		Me.btnSkill.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+		Me.btnSkill.Name = "btnSkill"
+		Me.btnSkill.Size = New System.Drawing.Size(115, 42)
+		Me.btnSkill.TabIndex = 4
+		Me.btnSkill.Text = "Skill"
+		Me.btnSkill.UseVisualStyleBackColor = False
+		'
+		'pbLogo
+		'
+		Me.pbLogo.BackColor = System.Drawing.Color.Transparent
+		Me.pbLogo.Image = Global.D4_Tools.My.Resources.Resources.d4_logo
+		Me.pbLogo.Location = New System.Drawing.Point(343, 3)
+		Me.pbLogo.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+		Me.pbLogo.Name = "pbLogo"
+		Me.pbLogo.Size = New System.Drawing.Size(89, 40)
+		Me.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+		Me.pbLogo.TabIndex = 1
+		Me.pbLogo.TabStop = False
 		'
 		'btnTracker
 		'
@@ -140,10 +143,28 @@ Partial Class FMain
 		Me.btnTracker.Text = "Tracker"
 		Me.btnTracker.UseVisualStyleBackColor = False
 		'
+		'btnDungeon
+		'
+		Me.btnDungeon.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+		Me.btnDungeon.FlatAppearance.BorderSize = 0
+		Me.btnDungeon.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.btnDungeon.Font = New System.Drawing.Font("Arial", 9.75!)
+		Me.btnDungeon.ForeColor = System.Drawing.Color.White
+		Me.btnDungeon.Location = New System.Drawing.Point(555, 0)
+		Me.btnDungeon.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+		Me.btnDungeon.Name = "btnDungeon"
+		Me.btnDungeon.Size = New System.Drawing.Size(115, 42)
+		Me.btnDungeon.TabIndex = 2
+		Me.btnDungeon.Text = "Dungeon"
+		Me.btnDungeon.UseVisualStyleBackColor = False
+		'
 		'pHomeContent
 		'
+		Me.pHomeContent.Controls.Add(Me.Panel6)
 		Me.pHomeContent.Controls.Add(Me.Panel5)
+		Me.pHomeContent.Controls.Add(Me.Label7)
 		Me.pHomeContent.Controls.Add(Me.Panel4)
+		Me.pHomeContent.Controls.Add(Me.PictureBox1)
 		Me.pHomeContent.Controls.Add(Me.Panel3)
 		Me.pHomeContent.Controls.Add(Me.Panel2)
 		Me.pHomeContent.Controls.Add(Me.Panel1)
@@ -161,52 +182,52 @@ Partial Class FMain
 		Me.pHomeContent.Location = New System.Drawing.Point(0, 43)
 		Me.pHomeContent.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
 		Me.pHomeContent.Name = "pHomeContent"
-		Me.pHomeContent.Size = New System.Drawing.Size(782, 428)
+		Me.pHomeContent.Size = New System.Drawing.Size(782, 499)
 		Me.pHomeContent.TabIndex = 2
 		'
 		'Panel5
 		'
 		Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-		Me.Panel5.Controls.Add(Me.lblCountdownBossFive)
-		Me.Panel5.Location = New System.Drawing.Point(408, 351)
+		Me.Panel5.Controls.Add(Me.lblCountdownHelltideOne)
+		Me.Panel5.Location = New System.Drawing.Point(518, 351)
 		Me.Panel5.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
 		Me.Panel5.Name = "Panel5"
-		Me.Panel5.Size = New System.Drawing.Size(187, 39)
+		Me.Panel5.Size = New System.Drawing.Size(187, 91)
 		Me.Panel5.TabIndex = 24
 		'
-		'lblCountdownBossFive
+		'lblCountdownHelltideOne
 		'
-		Me.lblCountdownBossFive.AutoSize = True
-		Me.lblCountdownBossFive.Font = New System.Drawing.Font("Arial", 13.0!)
-		Me.lblCountdownBossFive.ForeColor = System.Drawing.Color.White
-		Me.lblCountdownBossFive.Location = New System.Drawing.Point(58, 9)
-		Me.lblCountdownBossFive.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-		Me.lblCountdownBossFive.Name = "lblCountdownBossFive"
-		Me.lblCountdownBossFive.Size = New System.Drawing.Size(80, 21)
-		Me.lblCountdownBossFive.TabIndex = 14
-		Me.lblCountdownBossFive.Text = "00:00:00"
+		Me.lblCountdownHelltideOne.AutoSize = True
+		Me.lblCountdownHelltideOne.Font = New System.Drawing.Font("Arial", 13.0!)
+		Me.lblCountdownHelltideOne.ForeColor = System.Drawing.Color.White
+		Me.lblCountdownHelltideOne.Location = New System.Drawing.Point(58, 9)
+		Me.lblCountdownHelltideOne.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.lblCountdownHelltideOne.Name = "lblCountdownHelltideOne"
+		Me.lblCountdownHelltideOne.Size = New System.Drawing.Size(80, 21)
+		Me.lblCountdownHelltideOne.TabIndex = 14
+		Me.lblCountdownHelltideOne.Text = "00:00:00"
 		'
 		'Panel4
 		'
 		Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-		Me.Panel4.Controls.Add(Me.lblCountdownBossFour)
-		Me.Panel4.Location = New System.Drawing.Point(181, 351)
+		Me.Panel4.Controls.Add(Me.lblCountdownLegionOne)
+		Me.Panel4.Location = New System.Drawing.Point(75, 351)
 		Me.Panel4.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
 		Me.Panel4.Name = "Panel4"
-		Me.Panel4.Size = New System.Drawing.Size(187, 39)
+		Me.Panel4.Size = New System.Drawing.Size(187, 91)
 		Me.Panel4.TabIndex = 24
 		'
-		'lblCountdownBossFour
+		'lblCountdownLegionOne
 		'
-		Me.lblCountdownBossFour.AutoSize = True
-		Me.lblCountdownBossFour.Font = New System.Drawing.Font("Arial", 13.0!)
-		Me.lblCountdownBossFour.ForeColor = System.Drawing.Color.White
-		Me.lblCountdownBossFour.Location = New System.Drawing.Point(50, 9)
-		Me.lblCountdownBossFour.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-		Me.lblCountdownBossFour.Name = "lblCountdownBossFour"
-		Me.lblCountdownBossFour.Size = New System.Drawing.Size(80, 21)
-		Me.lblCountdownBossFour.TabIndex = 14
-		Me.lblCountdownBossFour.Text = "00:00:00"
+		Me.lblCountdownLegionOne.AutoSize = True
+		Me.lblCountdownLegionOne.Font = New System.Drawing.Font("Arial", 13.0!)
+		Me.lblCountdownLegionOne.ForeColor = System.Drawing.Color.White
+		Me.lblCountdownLegionOne.Location = New System.Drawing.Point(50, 9)
+		Me.lblCountdownLegionOne.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.lblCountdownLegionOne.Name = "lblCountdownLegionOne"
+		Me.lblCountdownLegionOne.Size = New System.Drawing.Size(80, 21)
+		Me.lblCountdownLegionOne.TabIndex = 14
+		Me.lblCountdownLegionOne.Text = "00:00:00"
 		'
 		'Panel3
 		'
@@ -280,7 +301,7 @@ Partial Class FMain
 		Me.Label4.AutoSize = True
 		Me.Label4.Font = New System.Drawing.Font("Arial", 9.0!)
 		Me.Label4.ForeColor = System.Drawing.Color.White
-		Me.Label4.Location = New System.Drawing.Point(209, 222)
+		Me.Label4.Location = New System.Drawing.Point(103, 222)
 		Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label4.Name = "Label4"
 		Me.Label4.Size = New System.Drawing.Size(133, 15)
@@ -328,7 +349,7 @@ Partial Class FMain
 		Me.Label5.AutoSize = True
 		Me.Label5.Font = New System.Drawing.Font("Arial", 9.0!)
 		Me.Label5.ForeColor = System.Drawing.Color.White
-		Me.Label5.Location = New System.Drawing.Point(449, 222)
+		Me.Label5.Location = New System.Drawing.Point(559, 222)
 		Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
 		Me.Label5.Name = "Label5"
 		Me.Label5.Size = New System.Drawing.Size(109, 15)
@@ -339,7 +360,7 @@ Partial Class FMain
 		'
 		Me.PictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
 		Me.PictureBox10.Image = Global.D4_Tools.My.Resources.Resources.Helltide
-		Me.PictureBox10.Location = New System.Drawing.Point(408, 240)
+		Me.PictureBox10.Location = New System.Drawing.Point(518, 240)
 		Me.PictureBox10.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
 		Me.PictureBox10.Name = "PictureBox10"
 		Me.PictureBox10.Size = New System.Drawing.Size(187, 105)
@@ -351,7 +372,7 @@ Partial Class FMain
 		'
 		Me.PictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
 		Me.PictureBox7.Image = Global.D4_Tools.My.Resources.Resources.Gathering_Legion
-		Me.PictureBox7.Location = New System.Drawing.Point(181, 240)
+		Me.PictureBox7.Location = New System.Drawing.Point(75, 240)
 		Me.PictureBox7.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
 		Me.PictureBox7.Name = "PictureBox7"
 		Me.PictureBox7.Size = New System.Drawing.Size(187, 105)
@@ -395,24 +416,58 @@ Partial Class FMain
 		Me.PictureBox2.TabIndex = 0
 		Me.PictureBox2.TabStop = False
 		'
-		'pbLogo
+		'Panel6
 		'
-		Me.pbLogo.BackColor = System.Drawing.Color.Transparent
-		Me.pbLogo.Image = Global.D4_Tools.My.Resources.Resources.d4_logo
-		Me.pbLogo.Location = New System.Drawing.Point(343, 3)
-		Me.pbLogo.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
-		Me.pbLogo.Name = "pbLogo"
-		Me.pbLogo.Size = New System.Drawing.Size(89, 40)
-		Me.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-		Me.pbLogo.TabIndex = 1
-		Me.pbLogo.TabStop = False
+		Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.Panel6.Controls.Add(Me.lblCountdownLegionTwo)
+		Me.Panel6.Location = New System.Drawing.Point(293, 351)
+		Me.Panel6.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+		Me.Panel6.Name = "Panel6"
+		Me.Panel6.Size = New System.Drawing.Size(187, 91)
+		Me.Panel6.TabIndex = 27
+		'
+		'lblCountdownLegionTwo
+		'
+		Me.lblCountdownLegionTwo.AutoSize = True
+		Me.lblCountdownLegionTwo.Font = New System.Drawing.Font("Arial", 13.0!)
+		Me.lblCountdownLegionTwo.ForeColor = System.Drawing.Color.White
+		Me.lblCountdownLegionTwo.Location = New System.Drawing.Point(50, 9)
+		Me.lblCountdownLegionTwo.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.lblCountdownLegionTwo.Name = "lblCountdownLegionTwo"
+		Me.lblCountdownLegionTwo.Size = New System.Drawing.Size(80, 21)
+		Me.lblCountdownLegionTwo.TabIndex = 14
+		Me.lblCountdownLegionTwo.Text = "00:00:00"
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Font = New System.Drawing.Font("Arial", 9.0!)
+		Me.Label7.ForeColor = System.Drawing.Color.White
+		Me.Label7.Location = New System.Drawing.Point(304, 222)
+		Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(166, 15)
+		Me.Label7.TabIndex = 26
+		Me.Label7.Text = "The Gathering Legions (next)"
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox1.Image = Global.D4_Tools.My.Resources.Resources.Gathering_Legion
+		Me.PictureBox1.Location = New System.Drawing.Point(293, 240)
+		Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(187, 105)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+		Me.PictureBox1.TabIndex = 25
+		Me.PictureBox1.TabStop = False
 		'
 		'FMain
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.Color.Black
-		Me.ClientSize = New System.Drawing.Size(782, 471)
+		Me.ClientSize = New System.Drawing.Size(782, 542)
 		Me.Controls.Add(Me.pHomeContent)
 		Me.Controls.Add(Me.pMenu)
 		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -421,6 +476,7 @@ Partial Class FMain
 		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
 		Me.Text = "D4 Tools"
 		Me.pMenu.ResumeLayout(False)
+		CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.pHomeContent.ResumeLayout(False)
 		Me.pHomeContent.PerformLayout()
 		Me.Panel5.ResumeLayout(False)
@@ -438,7 +494,9 @@ Partial Class FMain
 		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-		CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.Panel6.ResumeLayout(False)
+		Me.Panel6.PerformLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 
 	End Sub
@@ -459,8 +517,8 @@ Partial Class FMain
 	Friend WithEvents Label3 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents Label1 As Label
-	Friend WithEvents lblCountdownBossFive As Label
-	Friend WithEvents lblCountdownBossFour As Label
+	Friend WithEvents lblCountdownHelltideOne As Label
+	Friend WithEvents lblCountdownLegionOne As Label
 	Friend WithEvents lblCountdownBossThree As Label
 	Friend WithEvents lblCountdownBossTwo As Label
 	Friend WithEvents Panel5 As Panel
@@ -470,4 +528,8 @@ Partial Class FMain
 	Friend WithEvents Panel1 As Panel
 	Friend WithEvents pbLogo As PictureBox
 	Friend WithEvents btnTracker As Button
+	Friend WithEvents Panel6 As Panel
+	Friend WithEvents lblCountdownLegionTwo As Label
+	Friend WithEvents Label7 As Label
+	Friend WithEvents PictureBox1 As PictureBox
 End Class
--- D4_Tools/FMain.vb
+++ D4_Tools/FMain.vb
@@ -5,6 +5,7 @@ Imports D4_Tools.DataLayer
 Imports Windows.UI.Notifications
 
 Public Class FMain
+	'World Boss
 	Private countdownTimer As New Timer()
 	Private nextCountdownTimer As New Timer()
 	Private targetDateTime As DateTime
@@ -12,6 +13,15 @@ Public Class FMain
 	Private notificationShown As Boolean = False
 	Private actualBoss As String
 	Private nextBoss As String
+
+	'Legion
+	Private countdownLegionTimer As New Timer()
+	Private nextCountdownLegionTimer As New Timer()
+	Private legionNotificationShown As Boolean = False
+	Private legionTargetDateTime As DateTime
+	Private legionNextTargetDateTime As DateTime
+	Private legionTerritory As String
+	Private legionZone As String
 
 	Private Sub FMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		Me.pHomeContent.BackColor = Color.FromArgb(&H12, &H13, &H14)
@@ -67,13 +77,24 @@ Public Class FMain
 		Else
 		End If
 
-		' eventData anzeigen.
+		If eventData.ContainsKey("legion") Then
+			Dim legionData As Dictionary(Of String, Object) = DirectCast(eventData("legion"), Dictionary(Of String, Object))
+			legionTerritory = CStr(legionData("territory"))
+			legionZone = CStr(legionData("zone"))
+
+			legionTargetDateTime = UnixTimeStampToDateTime(CLng(legionData("expected")))
+			legionNextTargetDateTime = UnixTimeStampToDateTime(CLng(legionData("nextExpected")))
+
+			StartLegionCountdown()
+			StartNextLegionCountdown()
+		Else
+		End If
 	End Sub
 
 	Private Function UnixTimeStampToDateTime(unixTimeStamp As Long) As DateTime
 		Return New DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTimeStamp)
 	End Function
-
+#Region "World Boss"
 	Private Sub StartCountdown()
 		countdownTimer.Interval = 1000
 		AddHandler countdownTimer.Tick, AddressOf UpdateCountdown
@@ -100,7 +121,7 @@ Public Class FMain
 				lblCountdownBossOne.Text = "00:00:00"
 				countdownTimer.Stop()
 			End If
-		ElseIf nextBoss = "Avarive" Then
+		ElseIf nextBoss = "Avarice" Then
 			If remainingTime.Ticks > 0 Then
 				lblCountdownBossTwo.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
 				CheckNotification(remainingTime, nextBoss)
@@ -120,15 +141,16 @@ Public Class FMain
 			lblCountdownBossOne.Text = "00:00:00"
 			lblCountdownBossTwo.Text = "00:00:00"
 			lblCountdownBossThree.Text = "00:00:00"
-			lblCountdownBossFour.Text = "00:00:00"
-			lblCountdownBossFive.Text = "00:00:00"
+			lblCountdownLegionOne.Text = "00:00:00"
+			lblCountdownLegionTwo.Text = "00:00:00"
+			lblCountdownHelltideOne.Text = "00:00:00"
 		End If
 	End Sub
 	Private Sub UpdateCountdown(sender As Object, e As EventArgs)
 		Dim currentTime As DateTime = DateTime.UtcNow
 		Dim remainingTime As TimeSpan = targetDateTime - currentTime
 
-		If actualBoss = "Ashava" OrElse nextBoss = "Ashava" Then
+		If actualBoss = "Ashava" Then
 			If remainingTime.Ticks > 0 Then
 				lblCountdownBossOne.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
 				CheckNotification(remainingTime, actualBoss)
@@ -136,7 +158,7 @@ Public Class FMain
 				lblCountdownBossOne.Text = "00:00:00"
 				countdownTimer.Stop()
 			End If
-		ElseIf actualBoss = "Avarive" OrElse nextBoss = "Avarive" Then
+		ElseIf actualBoss = "Avarice" Then
 			If remainingTime.Ticks > 0 Then
 				lblCountdownBossTwo.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
 				CheckNotification(remainingTime, actualBoss)
@@ -144,7 +166,7 @@ Public Class FMain
 				lblCountdownBossTwo.Text = "00:00:00"
 				countdownTimer.Stop()
 			End If
-		ElseIf actualBoss = "The Wandering Death" OrElse nextBoss = "The Wandering Death" Then
+		ElseIf actualBoss = "The Wandering Death" Then
 			If remainingTime.Ticks > 0 Then
 				lblCountdownBossThree.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
 				CheckNotification(remainingTime, actualBoss)
@@ -156,8 +178,9 @@ Public Class FMain
 			lblCountdownBossOne.Text = "00:00:00"
 			lblCountdownBossTwo.Text = "00:00:00"
 			lblCountdownBossThree.Text = "00:00:00"
-			lblCountdownBossFour.Text = "00:00:00"
-			lblCountdownBossFive.Text = "00:00:00"
+			lblCountdownLegionOne.Text = "00:00:00"
+			lblCountdownLegionTwo.Text = "00:00:00"
+			lblCountdownHelltideOne.Text = "00:00:00"
 		End If
 	End Sub
 	Private Sub CheckNotification(remainingTime As TimeSpan, bossName As String)
@@ -177,6 +200,64 @@ Public Class FMain
 		notifyIcon.Visible = True
 		notifyIcon.ShowBalloonTip(5000)
 	End Sub
+#End Region
+#Region "Legion"
+	Private Sub StartLegionCountdown()
+		countdownLegionTimer.Interval = 1000
+		AddHandler countdownLegionTimer.Tick, AddressOf UpdateLegionCountdown
+		countdownLegionTimer.Start()
+		UpdateLegionCountdown(Nothing, EventArgs.Empty)
+	End Sub
+	Private Sub UpdateLegionCountdown(sender As Object, e As EventArgs)
+		Dim currentTime As DateTime = DateTime.UtcNow
+		Dim remainingTime As TimeSpan = legionTargetDateTime - currentTime
+
+		If remainingTime.Ticks > 0 Then
+			lblCountdownLegionOne.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
+			CheckNotification(remainingTime, legionTerritory, legionZone)
+		Else
+			lblCountdownLegionOne.Text = "00:00:00"
+			countdownLegionTimer.Stop()
+		End If
+	End Sub
+	Private Sub StartNextLegionCountdown()
+		nextCountdownLegionTimer.Interval = 1000
+
+		AddHandler nextCountdownLegionTimer.Tick, AddressOf UpdateNextLegionCountdown
+		nextCountdownLegionTimer.Start()
+		UpdateNextLegionCountdown(Nothing, EventArgs.Empty)
+	End Sub
+	Private Sub UpdateNextLegionCountdown(sender As Object, e As EventArgs)
+		Dim currentTime As DateTime = DateTime.UtcNow
+		Dim remainingTime As TimeSpan = legionNextTargetDateTime - currentTime
+
+		If remainingTime.Ticks > 0 Then
+			lblCountdownLegionTwo.Text = String.Format("{0:00}:{1:00}:{2:00}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds)
+			CheckNotification(remainingTime, legionTerritory, legionZone)
+		Else
+			lblCountdownLegionTwo.Text = "00:00:00"
+			nextCountdownLegionTimer.Stop()
+		End If
+	End Sub
+
+	Private Sub CheckNotification(remainingTime As TimeSpan, territory As String, zone As String)
+		If remainingTime.Hours = 0 AndAlso remainingTime.Minutes = 5 AndAlso remainingTime.Seconds = 0 AndAlso Not notificationShown Then
+			ShowNotification("Es sind nur noch 5 Minuten übrig!", territory, zone)
+			legionNotificationShown = True
+		Else
+			legionNotificationShown = False
+		End If
+	End Sub
+	Private Sub ShowNotification(message As String, territory As String, zone As String)
+		Dim notifyIcon As New NotifyIcon()
+		notifyIcon.Icon = SystemIcons.Information
+		notifyIcon.BalloonTipTitle = "Legion (Territory: '" & territory & "' - Zone: '" & zone & "')"
+		notifyIcon.BalloonTipText = message
+		notifyIcon.Visible = True
+		notifyIcon.ShowBalloonTip(5000)
+	End Sub
+#End Region
+#Region "Menu"
 	Private Sub btnTracker_Click(sender As Object, e As EventArgs) Handles btnTracker.Click
 		Me.pHomeContent.Visible = True
 	End Sub
@@ -191,4 +272,6 @@ Public Class FMain
 	Private Sub btnSkill_Click(sender As Object, e As EventArgs) Handles btnSkill.Click
 		Me.pHomeContent.Visible = False
 	End Sub
+#End Region
+
 End Class
